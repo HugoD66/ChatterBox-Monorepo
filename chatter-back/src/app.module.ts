dc4e99022c641a4d3e9bbf4070b2f3ca0896f2d4
Dessauw Hugo <dessauw.hugo@gmail.com>
2024-02-29 20:13:33 +0100

    install TypeORM, add docker-compose, connection DB OK ( no cors)

--- chatter-back/src/app.module.ts
+++ chatter-back/src/app.module.ts
@@ -1,10 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const dbConfig = {
+          type: `postgres` as const,
+          host: configService.get(`DB_HOST`),
+          port: +configService.get<number>(`DB_PORT`),
+          username: configService.get(`DB_USERNAME`),
+          password: configService.get(`DB_PASSWORD`),
+          database: configService.get(`DB_NAME`),
+          entities: [],
+          synchronize: true,
+        };
+        console.log(dbConfig);
+        return dbConfig;
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+
+}
