ecbb20c4037efd31c231287b8041b08e89410461
Dessauw Hugo <dessauw.hugo@gmail.com>
2024-04-03 19:01:08 +0200

    Test generation picture pixel, add blocs responsive on homepage

--- chatter-front/src/app/components/blocs/profil/profil.component.ts
+++ chatter-front/src/app/components/blocs/profil/profil.component.ts
@@ -1,14 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { UserModel } from '../../../models/user.model';
+import { MatButton } from '@angular/material/button';
+import * as jdenticon from 'jdenticon';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss',
 })
-export class ProfilComponent {
+export class ProfilComponent implements OnInit {
+  //TODO TEST CREATION IMAGE, A BOUGER DE PLACE
+  iconSvg: SafeHtml | undefined;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
   @Input() getMe!: BehaviorSubject<UserModel | null>;
+
+  ngOnInit(): void {
+    //TODO TEST CREATION IMAGE, A BOUGER DE PLACE
+    const value = 'user@example.com';
+    const size = 200;
+    this.iconSvg = this.sanitizer.bypassSecurityTrustHtml(
+      jdenticon.toSvg(value, size),
+    );
+  }
 }
