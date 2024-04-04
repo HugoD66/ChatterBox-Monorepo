29ed9fe6ce485054e959328cef86703093e8531c
Dessauw Hugo <dessauw.hugo@gmail.com>
2024-04-04 20:23:07 +0200

    general style

--- chatter-front/src/app/components/blocs/friend-list/friend-list.component.ts
+++ chatter-front/src/app/components/blocs/friend-list/friend-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-friend-list',
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: './friend-list.component.html',
   styleUrl: './friend-list.component.scss',
 })
--- chatter-front/src/app/components/blocs/last-message/last-message.component.ts
+++ chatter-front/src/app/components/blocs/last-message/last-message.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-last-message',
   standalone: true,
-  imports: [],
+  imports: [MatButton],
   templateUrl: './last-message.component.html',
   styleUrl: './last-message.component.scss',
 })
